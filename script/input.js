7013fd0c49d14d03a0ce41a054287ccfabae1fec
Vukasin Gostovic <vukasin@gostovic.de>
2021-03-22 01:06:25 +0100

    Removed bloat

--- script/input.js
+++ script/input.js
@@ -1,4 +1,3 @@
-//document.getElementById('contractAddressInput').value = '0x62bCc84bEA2c0aD32A5F1aC6842D636B3F2CAE9d';
 ethereum.on('accountsChanged', function (accounts) {
 	document.getElementById('contractBalanceInput').value = ethereum.selectedAddress;
 	document.getElementById('btn-disconnect').innerHTML = ethereum.selectedAddress;
