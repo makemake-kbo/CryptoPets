39e5719e76b624451a253e7590cedda960584f50
Vukasin Gostovic <vukasin@gostovic.de>
2021-04-01 18:47:31 +0200

    Added write specific inputs

---
+++ script/writeInput.js
@@ -0,0 +1,12 @@
+document.getElementById("petImg").style.visibility = 'hidden'; // visible
+
+
+ethereum.on('accountsChanged', function (accounts) {
+	document.getElementById('walletInputField').value = ethereum.selectedAddress;
+	document.getElementById('btn-disconnect').innerHTML = ethereum.selectedAddress;
+
+	if (ethereum.selectedAddress === null) {
+	document.getElementById('btn-connect').style = 'display: block';
+	document.getElementById('connected').style = 'display: none';
+	}
+});
