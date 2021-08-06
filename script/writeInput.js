17791ab1832de6d967766085b4c31d3bebb7e420
Vukasin Gostovic <vukasin@gostovic.de>
2021-08-06 11:58:28 +0200

    Updated write page

--- script/writeInput.js
+++ script/writeInput.js
@@ -5,7 +5,18 @@ ethereum.on('accountsChanged', function (accounts) {
 	document.getElementById('btn-disconnect').innerHTML = ethereum.selectedAddress;
 
 	if (ethereum.selectedAddress === null) {
-	document.getElementById('btn-connect').style = 'display: block';
-	document.getElementById('connected').style = 'display: none';
+		document.getElementById('btn-connect').style = 'display: block';
+		document.getElementById('connected').style = 'display: none';
 	}
 });
+
+const petNameInput = document.getElementById("nameInputField");
+const cardName = document.getElementById("cardName");
+
+petNameInput.addEventListener('change', (event) => {
+	if (petNameInput.value == "") {
+		cardName.innerHTML = "¯\\_(ツ)_/¯";
+	} else {
+		cardName.innerHTML = petNameInput.value;
+	}
+})
